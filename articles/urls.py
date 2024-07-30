566678ce98711e6a381b491db02f528a95855bbc
gusgh3 <poweremperor@naver.com>
2024-07-30 13:57:09 +0900

    Create기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views #현재 폴더와 같은 위치에서 가져옴 그래서 . 사용
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.index), 
+    path('', views.index, name='index'),
+    path('<int:id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
